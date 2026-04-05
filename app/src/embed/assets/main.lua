af40b4ac56857955ad356284490c2d9d2dc375db
okoklol1996 <yy2905860@gmail.com>
2026-04-05 09:10:05 +0200

    Update main.lua

--- app/src/embed/assets/main.lua
+++ app/src/embed/assets/main.lua
@@ -1,8 +1,14 @@
 function love.load()
+  love.window.setMode(0, 0, {fullscreen=true, resizable=true})
+  -- Get the actual phone screen size
+  sw = love.graphics.getWidth()
+  sh = love.graphics.getHeight()
+
+  love.graphics.setBackgroundColor(135/255, 135/255, 244/255)
   wf = require "libraries/windfield"
-  
   world = wf.newWorld(0, 0)
   
+  -- Collision Setup
   world:addCollisionClass('wall_left')
   world:addCollisionClass('wall_right')
   world:addCollisionClass('ground')
@@ -10,75 +16,49 @@ function love.load()
   
   player1_score = 0
   player2_score = 0
-  rectangle1 = world:newRectangleCollider(0, 300, 100, 200)
-  rectangle2 = world:newRectangleCollider(1200, 300, 100, 200)
-  roof = world:newRectangleCollider(-30, -30, 3000, 30)
-  ground = world:newRectangleCollider(-30, 770, 3000, 30)
-  wall_left = world:newRectangleCollider(-40, 0, 30, 3000)
-  wall_right = world:newRectangleCollider(1350, 0, 30, 3000)
-  circle = world:newCircleCollider(580, 350, 30)
+
+  -- PADDLES: Positioned based on screen width (sw) and height (sh)
+  rectangle1 = world:newRectangleCollider(50, sh/2, 60, 150)
+  rectangle2 = world:newRectangleCollider(sw - 50, sh/2, 60, 150)
+  
+  -- WALLS: Stretched to fit any screen size
+  roof = world:newRectangleCollider(sw/2, -10, sw, 20)
+  ground = world:newRectangleCollider(sw/2, sh + 10, sw, 20)
+  wall_left = world:newRectangleCollider(-10, sh/2, 20, sh)
+  wall_right = world:newRectangleCollider(sw + 10, sh/2, 20, sh)
+  
+  -- BALL: Starts in the exact center
+  circle = world:newCircleCollider(sw/2, sh/2, 20)
+  
+  -- Colors and Types
+  rectangles = { rectangle1, rectangle2 }
+  rectangles_color = {1, 0, 0}
+  circle_color = {1, 1, 0}
   
   wall_left:setCollisionClass('wall_left')
   wall_right:setCollisionClass('wall_right')
   ground:setCollisionClass('ground')
   roof:setCollisionClass('roof')
+  
   rectangle1:setType('kinematic')
   rectangle2:setType('kinematic')
   roof:setType('static')
   ground:setType('static')
   wall_left:setType('static')
   wall_right:setType('static')
+  
   circle:setRestitution(1)
-  circle:applyLinearImpulse(-1000, -250)
-end
-
-function love.update(dt)
-  world:update(dt)
-  if circle:enter('wall_left') then
-    circle:destroy()
-    player2_score = player2_score + 1
-    circle = world:newCircleCollider(580, 350, 30)
-    circle:setRestitution(1)
-    circle:applyLinearImpulse(1000, 0)
-  elseif circle:enter('wall_right') then
-    circle:destroy()
-    player1_score = player1_score + 1
-    circle = world:newCircleCollider(580, 350, 30)
-    circle:setRestitution(1)
-    circle:applyLinearImpulse(-1000, 0)
-end
-   p1x, p1y = rectangle1:getPosition()
-   p2x, p2y = rectangle2:getPosition()
-   if p1y < 100 then
-     rectangle1:setY(100)
-     rectangle1:setLinearVelocity(0, 0)
-   elseif p1y > 670 then
-     rectangle1:setY(670)
-     rectangle1:setLinearVelocity(0, 0)
-   end
-   if p2y < 100 then
-     rectangle2:setY(100)
-     rectangle2:setLinearVelocity(0, 0)
-   elseif p2y > 670 then
-     rectangle2:setY(670)
-     rectangle2:setLinearVelocity(0, 0)
-end
+  circle:applyLinearImpulse(-800, -200)
 end
 
 function love.touchpressed(id, x, y)
-    if y > 400 and x < 800 then
-      rectangle1:setLinearVelocity(0, 100)
-    elseif y < 400 and x < 800 then
-      rectangle1:setLinearVelocity(0, -100)
-    elseif y > 400 and x > 800 then
-      rectangle2:setLinearVelocity(0, 100)
-    elseif y < 400 and x > 800 then
-      rectangle2:setLinearVelocity(0, -100)
+    -- Use sw/2 to split the screen in half for Player 1 and Player 2
+    -- Use sh/2 to detect Top vs Bottom touches
+    if x < sw/2 then
+      if y > sh/2 then rectangle1:setLinearVelocity(0, 400)
+      else rectangle1:setLinearVelocity(0, -400) end
+    else
+      if y > sh/2 then rectangle2:setLinearVelocity(0, 400)
+      else rectangle2:setLinearVelocity(0, -400) end
     end
-end
-
-function love.draw()
-  love.graphics.print(player1_score, 500, 30, 0, 1.5)
-  love.graphics.print(player2_score, 700, 30, 0, 1.5)
-  world:draw()
 end
